1715172c06c572cd70df79fd69d28f7ce7fbb273
Pedro Xavier <pedroxxavier@hotmail.com>
2023-09-19 15:26:43 -0300

    Desenvolvendo o temporizador

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
+
 const musicaFocoInput = document.querySelector("#alternar-musica");
 const musica = new Audio("/sons/luna-rise-part-one.mp3");
+const start = new Audio("/sons/play.wav");
+const pause = new Audio("/sons/pause.mp3");
+const tempoFinalizado = new Audio("/sons/beep.mp3");
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
 musica.loop = true;
 
 musicaFocoInput.addEventListener("change", () => {
@@ -56,4 +65,33 @@ function alterarContexto(contexto) {
     default:
       break;
   }
+}
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    tempoFinalizado.play();
+    alert("Tempo finalizado!");
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log("Temporizador: " + tempoDecorridoEmSegundos);
+  console.log("Id: " + intervaloId);
+};
+
+startPauseBt.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+  if (intervaloId) {
+    pause.play();
+    zerar();
+    return;
+  }
+  start.play();
+  intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+  clearInterval(intervaloId);
+  intervaloId = null;
 }
